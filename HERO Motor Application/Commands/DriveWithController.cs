b43ef20e18faaae3e5aa7b122bfffed2c0aaff91
Weltgeist <narcisse118@gmail.com>
2022-12-29 17:48:39 -0500

    created DriveWithController with Iprocessable, ILoopable interface

---
+++ HERO Motor Application/Commands/DriveWithController.cs
@@ -0,0 +1,33 @@
+using CTRE.Phoenix;
+using CTRE.Phoenix.Tasking;
+
+namespace HERO_Motor_Application.Commands
+{
+    public class DriveWithController : IProcessable, ILoopable
+    {
+        public bool IsDone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnLoop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Process()
+        {
+
+        }
+    }
+}
