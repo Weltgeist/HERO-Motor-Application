d6c56a395657c20ee9da4c9a60f5f37f72bf1b48
Weltgeist <narcisse118@gmail.com>
2022-12-29 20:27:34 -0500

    added debugs

--- HERO Motor Application/Commands/DriveWithController.cs
+++ HERO Motor Application/Commands/DriveWithController.cs
@@ -3,6 +3,7 @@ using CTRE.Phoenix.Controller;
 using CTRE.Phoenix.Tasking;
 using HERO_Motor_Application.Extendeds;
 using HERO_Motor_Application.Subsystems;
+using Microsoft.SPOT;
 
 namespace HERO_Motor_Application.Commands
 {
@@ -19,28 +20,37 @@ namespace HERO_Motor_Application.Commands
 
         public bool IsDone()
         {
+
+            Debug.Print("IsDone? - Drive With Controller CMD");
             return false;
         }
 
         public void OnLoop()
         {
+
+            Debug.Print("Looping Drive With Controller CMD");
             Process();
         }
 
         public void OnStart()
         {
-           
+            Debug.Print("Starting Drive With Controller CMD");
         }
 
         public void OnStop()
         {
             driveTrain.stopMotors();
+            Debug.Print("Stopping Drive With Controller CMD");
         }
 
         public void Process()
         {
+
+            Debug.Print("Processing Drive With Controller CMD");
             float leftJoystickY = -1 * controller.GetAxis(LogictechMapping.leftJoystickY);
             float rightJoystickY = -1 * controller.GetAxis(LogictechMapping.rightJoystickY);
+            Debug.Print("LeftJoystickY Value:" + leftJoystickY.ToString());
+            Debug.Print("RightJoystickY Value:" + rightJoystickY.ToString());
             driveTrain.tankDrive(leftJoystickY, rightJoystickY);
         }
     }
